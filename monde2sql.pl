78cbc3bd75902f8a8f922fb6b8b5859dd01d797a
Etienne ANDRE <eti.andre@gmail.com>
2012-08-31 14:22:59 +0200

    support pour plusieurs mondes

---
+++ monde2sql.pl
@@ -0,0 +1,85 @@
+use strict;
+use warnings;
+use LWP::Simple;
+use Data::Dumper;
+use XML::Simple;
+use DBI;
+
+$|=1;
+my $monde=shift or die("Usage: $0 <bellios|aurora>\n");
+my $db = DBI->connect("dbi:SQLite:dbname=$monde.db");
+my $r;
+$db->do('DROP TABLE IF EXISTS joueurs');
+$db->do('DROP TABLE IF EXISTS alliances');
+$db->do('DROP TABLE IF EXISTS villes');
+$db->do('CREATE TABLE "alliances" (
+  "id" integer NOT NULL,
+  "nom" varchar(20) NOT NULL,
+  "pop" integer NOT NULL,
+  "classement" integer NOT NULL
+)');
+$db->do('CREATE TABLE "joueurs" (
+  "id" integer NOT NULL,
+  "nom" varchar(20) NOT NULL,
+  "pop" integer NOT NULL,
+  "classement" integer NOT NULL,
+  "id_alliance" integer NOT NULL,
+  PRIMARY KEY ("id")
+)');
+$db->do('CREATE TABLE "villes" (
+  "id" integer NOT NULL,
+  "terrain" integer NOT NULL,
+  "id_joueur" integer NOT NULL,
+  "nom" varchar(20) NOT NULL,
+  "pop" integer NOT NULL
+)');
+print "http://$monde.nemeria.com/ext\n";
+my $content=get("http://$monde.nemeria.com/ext");
+my $ref=XMLin($content, ForceArray=>['joueur']);
+my $joueurs=$ref->{'joueurs'}->{'joueur'};
+my $alliances=$ref->{'alliances'}->{'alliance'};
+my $villes=$ref->{'villes'}->{'ville'};
+print "pouet\n";
+$r=$db->prepare("INSERT into villes (id,terrain,id_joueur,nom,pop) VALUES (?,?,?,?,?);");
+print(keys($villes)." villes, ");
+foreach my $v (keys $villes) {
+    print STDERR ".";
+    $r->execute(
+        $v,
+        $villes->{$v}->{'terrain'},
+        $villes->{$v}->{'joueur'}[0],
+        $villes->{$v}->{'nom'},
+        $villes->{$v}->{'population'},
+    ) or print($v.Dumper($villes->{$v}));
+}
+print("\n");
+
+$r=$db->prepare("INSERT into alliances (id,nom,pop,classement) VALUES (?,?,?,?);");
+print(keys($alliances)." alliances,");
+foreach my $a (keys $alliances) {
+    print STDERR ".";
+    $r->execute(
+        $a,
+        $alliances->{$a}->{'nom'},
+        $alliances->{$a}->{'population'},
+        $alliances->{$a}->{'classement'},
+    ) or print($a.Dumper($alliances->{$a}));
+}
+print("\n");
+
+print(keys($joueurs)." joueurs.\n");
+$r=$db->prepare("INSERT into joueurs (id,nom,pop,classement,id_alliance) VALUES (?,?,?,?,?);");
+foreach my $j (keys $joueurs) {
+    print STDERR ".";
+    my $alli=$joueurs->{$j}->{'alliance'}->{'id'};
+    $alli=-1 unless ($alli);
+    $r->execute(
+        $j,
+        $joueurs->{$j}->{'nom'},
+        $joueurs->{$j}->{'population'},
+        $joueurs->{$j}->{'classement'},
+        $alli
+    );
+}
+
+
--- nemebot.py
+++ nemebot.py
@@ -9,14 +9,16 @@ import re
 import pickle
 import time
 import sqlite3
+import commands
 
 class Bot(ircbot.SingleServerIRCBot):
     def __init__(self):
         print "Connexion en cours..."
         ircbot.SingleServerIRCBot.__init__(self, [("irc.nemeria.com", 6667)],
                                            "NemeBot", "SQL NemeBot")
-        self.chan="#nemeria" # modifier ici le chan où se connecter
-        self.sql=sqlite3.connect("nemeria.db").cursor()
+        self.chan="#test" # modifier ici le chan où se connecter
+        self.monde="aurora"
+        self.sql=sqlite3.connect(self.monde+".db").cursor()
     def on_welcome(self, serv, ev):
         serv.join(self.chan)
         print "Réussi"
@@ -29,7 +31,26 @@ class Bot(ircbot.SingleServerIRCBot):
                 print "deco"
                 serv.disconnect("Bye")
                 exit()
-        if message.startswith("!joueur"):
+        if message.startswith("!monde "):
+            try:
+                arg=message.split("!monde ")[1].lower()
+                if arg=="" or re.search(r"[^\w]",arg):
+                    raise ValueError
+            except:
+                serv.notice(pseudo, "Usage: !monde <bellios|aurora>")
+            else:
+                if "bellios" in arg:
+                    self.monde="bellios"
+                elif "aurora" in arg:
+                    self.monde="aurora"
+               self.sql=sqlite3.connect(self.monde+".db").cursor()
+        elif message.startswith("!update"):
+            serv.privmsg(self.chan,"Mise à jour de la db de "+self.monde+"... à dans 5 minutes...")
+            serv.privmsg(self.chan,commands.getoutput("perl monde2sql.pl "+self.monde))
+            serv.privmsg(self.chan,"Fin de la mise à jour de "+self.monde+".")
+        elif message.startswith("!dbinfo"):
+            serv.privmsg(self.chan,"Monde courant: "+self.monde".")
+        elif message.startswith("!joueur"):
             try:
                 arg=message.split("!joueur ")[1].lower()
                 if arg=="" or re.search(r"[^\w\s]",arg):
@@ -43,7 +64,7 @@ class Bot(ircbot.SingleServerIRCBot):
                     self.sql.execute('SELECT * FROM alliances WHERE id=?',(j[4],))
                     a=self.sql.fetchone()
                     if a==None: a=(None,"pas d'alliance")
-                    serv.privmsg(self.chan, str(j[1])+" ("+str(a[1])+") a "+str(j[2])+" de population et est classé n°"+str(j[3])+". http://aurora.nemeria.com/profil?id="+str(j[0]))
+                    serv.privmsg(self.chan, str(j[1])+" ("+str(a[1])+") a "+str(j[2])+" de population et est classé n°"+str(j[3])+". http://"+self.monde+".nemeria.com/profil?id="+str(j[0]))
                     s="Villes: "
                     for v in self.sql.execute('SELECT * FROM villes WHERE id_joueur=?',(j[0],)):
                         s+=str(v[3])+"; "
@@ -61,7 +82,7 @@ class Bot(ircbot.SingleServerIRCBot):
                 try:
                     self.sql.execute('SELECT * FROM alliances WHERE nom LIKE ?',(arg,))
                     a=self.sql.fetchone()
-                    serv.privmsg(self.chan,"Alliance "+str(a[1])+": "+str(a[2])+" de population, classée n°"+str(a[3])+". http://aurora.nemeria.com/alliance?id="+str(a[0]))
+                    serv.privmsg(self.chan,"Alliance "+str(a[1])+": "+str(a[2])+" de population, classée n°"+str(a[3])+". http://"+self.monde+".nemeria.com/alliance?id="+str(a[0]))
                     s="Joueurs: "
                     for j in self.sql.execute('SELECT nom FROM joueurs WHERE id_alliance=?',(a[0],)):
                         s+=str(j[0])+"; "
@@ -81,12 +102,12 @@ class Bot(ircbot.SingleServerIRCBot):
                     v=self.sql.fetchone()
                     self.sql.execute('SELECT * FROM joueurs WHERE id=?',(str(v[2]),))
                     j=self.sql.fetchone()
-                    serv.privmsg(self.chan,str(v[3])+" ("+j[1]+") a "+str(v[4])+" de population. http://aurora.nemeria.com/carte?case="+str(v[0]))
+                    serv.privmsg(self.chan,str(v[3])+" ("+j[1]+") a "+str(v[4])+" de population. http://"+self.monde+".nemeria.com/carte?case="+str(v[0]))
                 except:
                     serv.privmsg(self.chan,"Pas trouvé.")
 
         elif message.startswith("!help"):
-            serv.notice(pseudo,"Commandes dispo pour NemeBot: !joueur !alliance !ville.")
+            serv.notice(pseudo,"Commandes dispo pour NemeBot: !joueur !alliance !ville !dbinfo !monde.")
     def on_join(self, serv, ev):
         pseudo = ev.source().split("!")[0].lower()
         if pseudo=="nemebot":
